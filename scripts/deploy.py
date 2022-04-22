33140b019bd4c13c55489fa323ad07abca02bd2a
davidchuaws <35304739+davidchuaws@users.noreply.github.com>
2022-04-22 18:17:19 +0800

    Initial commit

---
+++ scripts/deploy.py
@@ -0,0 +1,33 @@
+from scripts.helpful_scripts import get_account
+
+
+from scripts.helpful_scripts import get_account, get_contract
+from brownie import DappToken, TokenFarm, config, network
+from web3 import Web3
+
+KEPT_BALANCE = Web3.toWei(100, "ether")
+
+def main():
+    deploy_token_farm_and_dapp_token()
+
+def deploy_token_farm_and_dapp_token():
+    account = get_account()
+    dapp_token = DappToken.deploy({"from": account})
+    token_farm = TokenFarm.deploy(
+        dapp_token.address, 
+        {"from": account}, 
+        publish_source=config["networks"][network.show_active()]["verify"]
+    )
+    tx = dapp_token.transfer(
+        token_farm.address, 
+        dapp_token.totalSupply() - KEPT_BALANCE,
+        {"from": account}
+    )
+    tx.wait(1)
+    # dapp_token, weth_token, fau_token (DAI)
+    weth_token = get_contract("weth_token")
+    fau_token = get_contract("fau_token")
+    add_allowed_tokens(token_farm, dict_of_allowed_tokens, account)
+
+def add_allowed_tokens(token_farm, dict_of_allowed_tokens, account):
+    
